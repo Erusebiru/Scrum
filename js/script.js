0ae2d2ddaf1876f21693a507c77ca8f94fd59250
Erusebiru <Erusebiru@gmail.com>
2018-12-04 15:40:59 +0100

    Ventana de errores y proyecto de formulario

--- js/script.js
+++ js/script.js
@@ -1,19 +1,84 @@
+var countTime;
+var user = "ProductOwner";
+newProyect(user);
+proyectLink();
 
-var personas = [];
+//Función que añade el evento click a los links de proyectos
+function proyectLink(){
+	var links = document.getElementsByName("proyecto");
+	links.forEach(function(element){
+		element.addEventListener("click",createErrorWindow);
+	});
+}
 
-class Persona{
-	constructor(id,name,tipo){
-		this.id = id;
-		this.name = name;
-		this.tipo = tipo;
+//Función que crea la ventana de error y los diferentes errores
+function createErrorWindow(){
+	var texto = "ERROR FATAL!!";
+	var parent = document.querySelector(".error");
+	var div = addElement(parent,"div",undefined,["class=image-error"]);
+	var image = addElement(div,"img",undefined,["src=images/error.png"]);
+	var span = addElement(div,"span",texto,["class=error-message"]);
+	var span = document.createElement("span");
+
+	document.querySelector(".window-message").style.display = "block";
+	resetAnimation(parent);
+	setTimer();
+}
+
+//Función que resetea la animación clonando el objeto y reemplazándolo por sí mismo. Cuando esto ocurre la animación inicia de cero.
+function resetAnimation(error){
+	var newElement = error.cloneNode(true);
+	error.parentNode.replaceChild(newElement,error);
+}
+
+//Función que crea un timer que, tras 10 segundos, oculta de nuevo la ventana de error y elimina los errores creados.
+//Se eliminan para que la próxima vez que se genere un error sin recargar la página no aparezcan.
+function setTimer(){
+	clearTimeout(countTime);
+	countTime =	setTimeout(function (){
+		document.querySelector(".window-message").style.display = "none";
+		var parent = document.querySelector(".error");
+		var childs = document.querySelectorAll(".image-error");
+		childs.forEach(function(element){
+			parent.removeChild(element);
+		});
+	}, 10000);
+}
+
+//Función que crea el botón de AddProyect, que sólo será visible si el usuario es de tipo ProductOwner
+function newProyect(user){
+	if(user == "ProductOwner"){
+		var parent = document.querySelector(".proyect-table");
+		addElement(parent,"button","Add Proyect",["onclick=showForm();","name=addProyect"])
 	}
 }
 
-var grupos = [];
 
-class Grupo{
-	constructor(id,name){
-		this.id = id;
-		this.name = name;
+//Función que muestra el formulario para crear un nuevo proyecto. Este formulario sólo se creará cuando se clicke el botón de AddProyect
+function showForm(){
+	var parent = document.querySelector(".contenedor");
+	var div = addElement(parent,"div",undefined,["class=proyect-list"]);
+	var form = addElement(div,"form");
+	addElement(form,"input",undefined,["type=text"]);
+}
+
+
+//Función para crear nuevos elementos. Podrán tener un texto y diferentes atributos que se enviarán en forma de array
+//Devuelve el elemento creado para poder concatenar creación de elementos
+function addElement(parent, child, text,attributes){
+	var childElement = document.createElement(child);
+	if(text != undefined){
+		var contenido = document.createTextNode(text);
+		childElement.appendChild(contenido);
 	}
+
+	if(attributes != undefined && attributes instanceof Array){
+		for(var i=0;i<attributes.length;i++){
+			var attrName = attributes[i].split("=")[0];
+			var attrValue = attributes[i].split("=")[1];
+			childElement.setAttribute(attrName,attrValue);
+		}
+	}
+	parent.appendChild(childElement);
+	return childElement;
 }
--- proyectos.php
+++ proyectos.php
@@ -4,7 +4,7 @@
 	<title></title>
 	<link type="text/css" rel="stylesheet" href="css/style.css"/>
 	<script type="text/javascript" src="js/script.js" defer></script>
-	<script type="text/javascript" src="js/clases.js"></script>
+	<!--<script type="text/javascript" src="js/clases.js"></script>-->
 </head>
 <body>
 	<div class="contenedor">
@@ -13,9 +13,9 @@
 			<div class="proyect-table">
 				<ul>
 				<?
-					for($i=1;$i<30;$i++){
+					for($i=1;$i<5;$i++){
 						?><li>
-							<a href="#">Proyecto <?=$i?></a>
+							<a href="#" name="proyecto">Proyecto <?=$i?></a>
 						</li><?
 					}
 				?>
@@ -26,9 +26,9 @@
 
 	<div class="window-message">
 		<div class="error">
-			<div class="image-error">
-				<span id="error-message">ERROR</span>
-			</div>
+			<!--<div class="image-error">
+				<span name="error-message">ERROR</span>
+			</div>-->
 		</div>
 	</div>
 </body>
