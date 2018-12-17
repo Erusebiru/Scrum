5f692938e1fb6fb04f96e4092b738cb07910f0fe
KhalidAlouan <kalouan@iesesteveterradas.cat>
2018-12-17 18:05:14 +0100

    actualizado

--- recuperarPasswordValidar.php
+++ recuperarPasswordValidar.php
@@ -39,9 +39,14 @@
    // $message = "PHP mail works just fine";
    //$headers = "From:ubuntu@gfjkghjkflk.com";
    // mail($to,$subject,$message, $headers);
+	// mail("kalouan@iesesteveterradas.cat","probando","probando mail","from: ubuntu@blablaslba.com");
+   // echo "Se ha enviado un mensaje a tu cuenta de correo, para cambiar la contraseña de tu usuario";
+		
    include 'connection.php';
 
    $consulta_correo ="SELECT email from usuarios where nombre_usuario'".$user."';";
+   $user_comprovar = mysqli_num_rows(mysqli_query($consulta_correo));
+
 
    mail($consulta_correo,"Recuperar Contraseña","Ve a la pagina siguiente para cambiar la contraseña de tu usuario: http://www.khalidomain.ml/Scrum/cambiarPassword.php","from: ubuntu@blablaslba.com");
    echo "Se ha enviado un email a tu cuenta de correo electronico para el cambio de contraseña.";
