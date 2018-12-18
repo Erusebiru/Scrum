daa5a79e9cb02623c51357ee9fb7016214e82bd5
Erusebiru <Erusebiru@gmail.com>
2018-12-18 15:27:02 +0100

    Recuperar password

--- cambiarPassword.php
+++ cambiarPassword.php
@@ -19,51 +19,78 @@
 <body>
 	<?php
 	if (isset($_POST['Password'])) {
-		include 'connection.php';
-		/*$consulta_token="UPDATE usuarios set password where token='".$token."'";
-		$query = "UPDATE usuarios SET token = '$token' WHERE nombre_usuario = '$user'";*/
+		
+    updatePassword($_GET['token'],$_POST['Password']);
+		
+		
     echo "CORRECTO";
-	}else{	
-    
+	}else{
+    if(isset($_GET['token'])){
+      if(checkToken($_GET['token'])){
+        ?><div class="container">
+          <div class = "row" style = "width:100%;">
+            <div id="divcabecera" class = "col s12 m12 l12" >
+              <nav>
+                <div class = "nav-wrapper">
+                  <a href = "#" class = "brand-logo nombrelogo">Cambiar Password</a>
+                  <a href="#!" class="brand-logo center"><img src="https://www.logolynx.com/images/logolynx/15/1588b3eef9f1607d259c3f334b85ffd1.png"></a>
+                </div>
+              </nav>
+            </div>
+            <div class="parallax"><img src="https://cdn.pixabay.com/photo/2016/06/02/02/38/mesh-1430108_960_720.png"></div>
+            <div class="s12 m12 l12"><br> <br></div>    
+            <div id="formulario" class="col m8 s8 l8 offset-m2 offset-s2 offset-l2 center z-depth-3">
+              <form class = "col l12" method="post" action="#" id="formpass" >
+                <div align="center" class = "center input-field col l12">
+                  <input placeholder = "Password" name = "Password" id="primerPassword" type = "password" class = "active validate" required />
+                  <label for = "username">Password</label>
+                </div>
+                <div align="center" class = "center input-field col l12">
+                  <input placeholder = "Password" name = "Password2" id="segundoPassword"type = "password" class = "active validate" required />
+                  <label for = "username">Password</label>
+                </div>
+
+                <div class = "col l12">
+              </form>
+              <div class="button" onclick="compararPassword()">Cambiar Password</div>
+            </div>
+          </div>
+        </div>
+        <div class="window-message">
+          <div class="error"></div>
+        </div><?
+      }else{
+        echo "<h5>ENLACE DE RECUPERACIÓN INCORRECTO</h5>";
+        exit;
+      }
+    }
 	}
 ?>
-    <div class="container">
-      <div class = "row" style = "width:100%;">
-        <div id="divcabecera" class = "col s12 m12 l12" >
-          <nav>
-            <div class = "nav-wrapper">
-              <a href = "#" class = "brand-logo nombrelogo">Cambiar Password</a>
-              <a href="#!" class="brand-logo center"><img src="https://www.logolynx.com/images/logolynx/15/1588b3eef9f1607d259c3f334b85ffd1.png"></a>
-            </div>
-          </nav>
-        </div>
-        <div class="parallax"><img src="https://cdn.pixabay.com/photo/2016/06/02/02/38/mesh-1430108_960_720.png"></div>
-        <div class="s12 m12 l12"><br> <br></div>    
-        <div id="formulario" class="col m8 s8 l8 offset-m2 offset-s2 offset-l2 center z-depth-3">
-          <form class = "col l12" method="post" action="#" id="formpass" >
-            <div align="center" class = "center input-field col l12">
-              <input placeholder = "Password" name = "Password" id="primerPassword" type = "password" class = "active validate" required />
-              <label for = "username">Password</label>
-            </div>
-            <div align="center" class = "center input-field col l12">
-              <input placeholder = "Password" name = "Password2" id="segundoPassword"type = "password" class = "active validate" required />
-              <label for = "username">Password</label>
-            </div>
 
-            <div class = "col l12">
-          </form>
-          <div class="button" onclick="compararPassword()">Cambiar Password</div>
-        </div>
-      </div>
-      </div>
-    </div>
-    <div class="window-message">
-      <div class="error"></div>
-    </div>
-	
-	 
-	
+  <?
 
+    function checkToken($token){
+      include "connection.php";
+      $query = "SELECT nombre_usuario FROM usuarios WHERE token = '$token'";
+      $result = mysqli_query($conn,$query);
+      $check = mysqli_num_rows($result);
+      if($check != 0){
+        return true;
+      }
+      return false;
+    }
+
+    function updatePassword($token,$password){
+      include 'connection.php';
+      $query = "UPDATE usuarios set password = sha2('".$password."',512) WHERE token='".$token."'";
+      if (mysqli_query($conn, $query)) {
+        echo "Usuario actualizado correctamente";
+      } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($conn);
+      }
+    }
+
+  ?>
 
 </body>
 </html>
